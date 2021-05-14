60006c50b5aaee971c29f00b66cdd9c260990736
Andrew <Andrewringold@gmail.com>
2021-05-14 14:43:39 -0500

    Started Block Class

---
+++ Shapes/Block.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Block {
+    private int xCoord;
+    private int yCoord;
+    private int[] rgbColor = new int[3];
+
+    public Block(int x, int y, int[] rgb) {
+        this.xCoord = x;
+        this.yCoord = y;
+        this.rgbColor = rgb;
+    }
+}
