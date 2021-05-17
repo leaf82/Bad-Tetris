0898cfd838821c28653713562aecfa54c20270a6
DocHaagenDazs <84102993+DocHaagenDazs@users.noreply.github.com>
2021-05-17 15:26:58 -0500

    Update DisplayGraphics.java

--- Tetris_Shapes/DisplayGraphics.java
+++ Tetris_Shapes/DisplayGraphics.java
@@ -14,12 +14,11 @@ public class DisplayGraphics extends  Canvas{
     }  
     */
     
-    public void paint(Graphics g) {  
-        g.drawString("Hello",40,40);  
-        setBackground(Color.WHITE);  
+    public void paint(Graphics g) {    
+        setBackground(Color.decode("#000000"));  
         g.fillRect(130, 30,100, 80);  
-        g.drawOval(30,130,50, 60);  
-        setForeground(Color.RED);  
+        g.fillRect(230,30,50, 10);  
+        setForeground(Color.BLUE);  
         g.fillOval(130,130,50, 60);  
         g.drawArc(30, 200, 40,50,90,60);  
         g.fillArc(30, 130, 40,50,180,40);  
