41891802293b6204414bdf035707b22133cf70f2
DocHaagenDazs <84102993+DocHaagenDazs@users.noreply.github.com>
2021-05-20 14:31:22 -0500

    Update Screen.java

--- Tetris_Shapes/Screen.java
+++ Tetris_Shapes/Screen.java
@@ -4,16 +4,24 @@ import java.awt.*;
 public class Screen extends  JPanel{    
     public void paint(Graphics g) {    
         setBackground(Color.GRAY);
-
-        g.fillRect(100, 100, 100, 100);
-        setForeground(Color.BLUE);
+        final int spacer = 30;
+        for(int i = getHeight()/2 - (spacer * 10); i < getHeight()/2 + (spacer * 10); i += spacer)
+        {
+            for(int x = getWidth()/2 - (spacer * 5); x < getWidth()/2 + (spacer * 5); x += (spacer))
+            {
+                g.drawRect(x, i, (spacer), (spacer));
+            }
+        }
+        
+        setForeground(Color.black);
     }
     //10x20
     public static void main(String[] args) {
         Screen m=new Screen();
         JFrame f=new JFrame();
         f.add(m);
-        f.setSize(400,400);
+        final int side = 800;
+        f.setSize(800, 800);
         f.setVisible(true);
     }
 }
