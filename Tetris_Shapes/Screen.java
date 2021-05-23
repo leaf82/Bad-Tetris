78e13a78788537bac0dd7d4cb066d853263ca09e
DocHaagenDazs <84102993+DocHaagenDazs@users.noreply.github.com>
2021-05-23 16:30:56 -0500

    Update Screen.java
    added at timer and a block that should move down but just blacks out a whole line

--- Tetris_Shapes/Screen.java
+++ Tetris_Shapes/Screen.java
@@ -1,11 +1,25 @@
 package Tetris_Shapes;
 import javax.swing.*;
 import java.awt.*;
-public class Screen extends  JPanel{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Screen extends  JPanel implements ActionListener{
     private final int spacer = 30;
+    Timer time = new Timer(5, this);
+    int y = 0, yVel = 2;
+
     public int getSpacer()
     {
         return spacer;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        y += yVel;
+        
+        repaint();
     }
 
     public void paint(Graphics g) {    
@@ -17,7 +31,16 @@ public class Screen extends  JPanel{
                 g.drawRect(x, i, (spacer), (spacer));
             }
         }
+        g.fillRect(getWidth()/2 - (spacer * 5), getHeight()/2 - (spacer * 10) + y, spacer, spacer);
         setForeground(Color.black);
+
+        if(y + getHeight()/2 - (spacer * 10) == (getHeight()/2 + (spacer * 10)) - 30)
+        {
+            time.stop();
+        } else {
+            time.start();
+        }
+        
     }
     //10x20
     public static void main(String[] args) {
