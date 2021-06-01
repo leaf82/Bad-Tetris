cdeb687476bdb9308b6addf99dc83ca7568f619c
Andrew <Andrewringold@gmail.com>
2021-06-01 14:08:11 -0500

    Enum for choosing shape

--- Shape.java
+++ Shape.java
@@ -1,12 +1,14 @@
 import java.awt.*;
 import javax.swing.*;
 
+enum blocks {STR, FRTL, BCKL, SQR, FRTZ, BCKZ, TBLK};
+
 public class Shape {
     // private int x;
     // private int y;
     // private int side;
     // private Color color;
-    // private Graphics g;
+    private Graphics g;
 
     // public Block(int x, int y, int side, Color color, Graphics g) {
     //     this.x = x;
@@ -19,11 +21,34 @@ public class Shape {
     //     g.drawRect(x, y, side, side);
     // }
 
-    private enum blocks {straight, frontL, backL, square, BackZ, frontZ, tBlock};
     private int rotation = 0; // count from 0 to 3 for each rotation of the block
 
-    public Shape(String block, Grpahics g) {
-
+    public Shape(blocks block, Graphics g) {
+        blocks b = block;
+        this.g = g;
+        switch(b) {
+            case STR:
+                System.out.println("Straight");
+                break;
+            case FRTL:
+                System.out.println("Front L");
+                break;
+            case BCKL:
+                System.out.println("Back L");
+                break;
+            case SQR:
+                System.out.println("Square");
+                break;
+            case FRTZ:
+                System.out.println("Front Z");
+                break;
+            case BCKZ:
+                System.out.println("Backwards Z");
+                break;
+            case TBLK:
+                System.out.println("T Block");
+                break;
+        }
     }
 
     public void draw(Graphics g) {
