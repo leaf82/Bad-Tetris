2a2a5f47aecb371ee087a10e7b0a5e6c57b40c3c
DocHaagenDazs <84102993+DocHaagenDazs@users.noreply.github.com>
2021-06-03 14:55:46 -0500

    straight spawns in the right spot

--- Screen.java
+++ Screen.java
@@ -21,22 +21,21 @@ public class Screen extends  JPanel implements ActionListener{
         repaint();
     }
 
-    public void paintComponent(Graphics g) {   
+    public void paintComponent(Graphics g) { 
         super.paintComponent(g); // Properly refreshes
         setBackground(Color.GRAY); // Background color
-        
 
+        //10x20
         //Prints a grid for the blocks to move across
         for(int y = getHeight()/2 - (spacer * 10); y < getHeight()/2 + (spacer * 10); y += spacer)
         {
             for(int x = getWidth()/2 - (spacer * 5); x < getWidth()/2 + (spacer * 5); x += (spacer))
             {
-                Grid.draw(x, y, spacer, g);
+               Grid.draw(x, y, spacer, g);
             }
         }
-
         /* Making a line piece using a var for the y value
-           to allow it to be redrawn and moved
+        to allow it to be redrawn and moved
         
         g.fillRect(getWidth()/2 - (spacer * 5), getHeight()/2 - (spacer * 10) + y, spacer, spacer);
         g.fillRect(getWidth()/2 - (spacer * 5), getHeight()/2 - (spacer * 10) + y, spacer * 2, spacer);
@@ -48,10 +47,10 @@ public class Screen extends  JPanel implements ActionListener{
         if (!time.isRunning()) {
             ran = (int)(Math.random() * (6));
         } 
-            
+        
         // All of the different classes using their draw methods
         if (ran == 0){
-            Straight.draw(getWidth()/2 - (spacer), getHeight()/2 - (spacer * 10) + y, g);
+            Straight.draw(getWidth()/2 - (spacer), getHeight()/2 - (spacer * 10) + y + 30, g);
         } else if (ran == 1){
             Tblock.draw(getWidth()/2 - spacer, getHeight()/2 - (spacer * 10) + y, g);
         } else if (ran == 2){
@@ -65,26 +64,26 @@ public class Screen extends  JPanel implements ActionListener{
         } else {
             BackL.draw(getWidth()/2 - spacer, getHeight()/2 - (spacer * 10) + y, g);
         }
-            
+                
         setForeground(Color.black); // Color of things drawn
 
         // Starts the timer causing the blocks to fall
-        if(y + getHeight()/2 - (spacer * 10) == (getHeight()/2 + (spacer * 10)) - 30)
+        if(y + getHeight()/2 - (spacer * 10) == (getHeight()/2 + (spacer * 10)) - (spacer * 2))
         {
+            y = 0;
+            ran = 0;
             time.stop();
         } else {
             time.start();
         }
     }
 
-    //10x20
     public static void main(String[] args) {
         Screen m=new Screen();
         JFrame f=new JFrame();
         int side = 800;
-        f.add(m);
         f.setSize(side, side);
         f.setVisible(true);
-
+        f.add(m);
     }
 }
--- Tetris_Shapes/Straight.java
+++ Tetris_Shapes/Straight.java
@@ -11,7 +11,7 @@ public class Straight{
     
     public static void draw(int x, int y, Graphics g){
         g.fillRect(x - 30, y, 30, 30); //far left
-        g.fillRect(x , y, 30, 30); // left
+        g.fillRect(x , y , 30, 30); // left
         g.fillRect(x + 30, y, 30, 30); //right
         g.fillRect(x + 60, y, 30, 30); //far right
     }
